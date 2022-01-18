b8eea8b2c1300256e131912eb76e48aae3efdb22
Bruno Cheles <bruno.cheles@outlook.com>
2022-01-18 00:57:17 -0300

    Ajustes do Exercício#1 e final do #2
    Feito alguns ajustes no exercício#1 e finalização do exercício#2.

---
+++ Exercício#2/Exercício#2.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <conio.h>
+
+int SequenciaFibonacci (int num) {
+    int j = 1, i;
+    for(i = 0;(i <= num || j <= num);) {
+        if (num == j || num == i) {
+            return 1;
+        }
+        i = i + j;
+        j = j + i;
+    }
+    return 0;
+}
+
+int main(void) {
+    int numero;
+    printf("Digite um numero:");
+    scanf("%d",&numero);
+
+    if(SequenciaFibonacci(numero)) {
+        printf("\n Este numero pertence a sequencia de Fibonacci.");
+    }else {
+        printf("\n Este numero nao pertence a sequencia de Fibonacci.");
+    }
+}
