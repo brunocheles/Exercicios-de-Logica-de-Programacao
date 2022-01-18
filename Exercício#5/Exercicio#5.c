a6c958db9a02ab396db6ef4d9d9c25eae7584ef6
Bruno Cheles <bruno.cheles@outlook.com>
2022-01-18 02:53:10 -0300

    Finalização do Exercício#5
    Depois de muito quebrar a cabeça com C, finalizei o exercício 5.

---
+++ Exercício#5/Exercicio#5.c
@@ -0,0 +1,24 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <locale.h>
+
+int main(void) {
+    setlocale(LC_ALL, "Portuguese");
+    char frase[150];
+    int tamanhoFrase = 0, i = 0;
+
+    printf("Digite uma frase(Máximo 150 caracteres): ");
+    gets(frase);
+
+
+    tamanhoFrase = strlen(frase);
+
+    printf("\n Frase Invertida: ");
+    for (i = tamanhoFrase; i >= 0; i--) {
+        printf("%c",frase[i]);
+    }
+
+    printf("\n Frase original: %s \n",frase);
+    getch();
+}
