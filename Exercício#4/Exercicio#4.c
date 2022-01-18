b28e20241004e31305328c8d207f1d1a45c06953
Bruno Cheles <bruno.cheles@outlook.com>
2022-01-18 01:51:33 -0300

    Finalização Exercício#4
    Resposta do exercício#4

---
+++ Exercício#4/Exercicio#4.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main (void) {
+    setlocale(LC_ALL, "Portuguese");
+    printf(" No momento exato em que eles se cruzarem, eles estarao a mesma distancia de Ribeirao Preto.\n");
+    printf(" Cheguei a essa conclusão pela interpretação do texto do exercício, na parte: \n");
+    printf("\t'Quando eles se cruzarem na rodovia, qual estará mais próximo a cidade de Ribeirão Preto?'\n");
+}
