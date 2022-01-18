4e6ca7e3cd025d780eb8e47fcadef45b43408c49
Bruno Cheles <bruno.cheles@outlook.com>
2022-01-18 00:10:28 -0300

    Exercício nº 1 final
    Finalização do primeiro exercício.

---
+++ Testes.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+int Soma() {
+    int SOMA = 0, INDICE = 13, K = 0;
+
+    while(K < INDICE){
+        K = K + 1;
+        SOMA = SOMA + K;
+    };
+    return SOMA;
+}
+
+int main(void) {
+    int valorSoma = Soma();
+    printf("Soma = %d \n", valorSoma);
+
+    return 0;
+}
